012832c630c2b2ae9fc1b308adc9741bdce8c12c
Sujit <sb2011ams@gmail.com>
2024-10-23 14:16:25 +0000

    Game development with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+import string
+import random
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        if not word:
+            return False
+
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
---
+++ tests/test_game.py
@@ -0,0 +1,38 @@
+#tests/test_game.py
+from longest_word.game import Game
+import string
+
+class TestGame:
+    def test_game_initialization(self):
+            # setup
+            new_game = Game()
+
+            # exercise
+            grid = new_game.grid
+
+            # verify
+            assert isinstance(grid, list)
+            assert len(grid) == 9
+            for letter in grid:
+                assert letter in string.ascii_uppercase
+
+            # teardown
+            pass
+
+    def test_empty_word_is_invalid(self):
+            # setup
+            new_game = Game()
+            # verify
+            assert new_game.is_valid('') is False
+
+    def test_is_valid(self):
+            # setup
+            new_game = Game()
+            test_grid = 'KWEUEAKRZ'
+            test_word = 'EUREKA'
+            # exercice
+            new_game.grid = list(test_grid) # Force the grid to a test case
+            # verify
+            assert new_game.is_valid(test_word) is True
+            # teardown
+            assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
