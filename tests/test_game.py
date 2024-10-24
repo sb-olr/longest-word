3f61b3fd86a5eb6f34b0fef00ddb8751be07437e
Sujit <sb2011ams@gmail.com>
2024-10-24 09:08:21 +0000

    Feature complete: Dictionary check of attempt

--- tests/test_game.py
+++ tests/test_game.py
@@ -16,9 +16,6 @@ class TestGame:
             for letter in grid:
                 assert letter in string.ascii_uppercase
 
-            # teardown
-            pass
-
     def test_empty_word_is_invalid(self):
             # setup
             new_game = Game()
@@ -37,8 +34,8 @@ class TestGame:
             # teardown
             assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
 
-def test_unknown_word_is_invalid(self):
-        """A word that is not in the English dictionary should not be valid"""
-        new_game = Game()
-        new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
-        assert new_game.is_valid('FEUN') is False
+    def test_unknown_word_is_invalid(self):
+            """A word that is not in the English dictionary should not be valid"""
+            new_game = Game()
+            new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+            assert new_game.is_valid('FEUN') is False
